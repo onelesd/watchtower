7715e4093879e36ea8dcda8eb55ef72b0d7f4d38
Charlie Revett <charlierevett@gmail.com>
2015-04-07 21:01:03 +0100

    Close #15 - Loop support

--- tests/configParserTest.js
+++ tests/configParserTest.js
@@ -2,35 +2,67 @@ var assert       = require("assert");
 var configParser = require("../lib/watchtower/configParser.js");
 
 describe("ConfigParser", function() {
-  var path   = "./tests/fixtures/config.yaml";
-  var parser = new configParser(path);
-
   describe("#new()", function() {
+    var path   = "./tests/fixtures/config.yaml";
+    var parser = new configParser(path);
+
     it("should set path instance variable.", function() {
       assert.equal(path, parser.path);
     });
   });
 
   describe("#parse()", function() {
-    var config = {
-      endpoints: [
-        {
-          site: "BBC",
-          url: "http://bbc.co.uk/news"
-        },
-        {
-          site: "BBC",
-          url: "http://bbc.co.uk/music"
-        },
-        {
-          site: "Guardian",
-          url: "http://theguardian.com/uk/sport"
-        }
-      ]
-    };
+    describe("non-looped", function() {
+      var config = {
+        loop: false,
+        endpoints: [
+          {
+            site: "BBC",
+            url: "http://bbc.co.uk/news"
+          },
+          {
+            site: "BBC",
+            url: "http://bbc.co.uk/music"
+          },
+          {
+            site: "Guardian",
+            url: "http://theguardian.com/uk/sport"
+          }
+        ]
+      };
+      var path   = "./tests/fixtures/config.yaml";
+      var parser = new configParser(path);
 
-    it("should parse YAML.", function() {
-      assert.deepEqual(config, parser.parse());
+      it("should parse YAML.", function() {
+        assert.deepEqual(config, parser.parse());
+      });
+    });
+
+    describe("looped", function() {
+      var config = {
+        loop: true,
+        interval: 5000,
+        endpoints: [
+          {
+            site: "BBC",
+            url: "http://bbc.co.uk/news"
+          },
+          {
+            site: "BBC",
+            url: "http://bbc.co.uk/music"
+          },
+          {
+            site: "Guardian",
+            url: "http://theguardian.com/uk/sport"
+          }
+        ]
+      };
+      var path   = "./tests/fixtures/loop_config.yaml";
+      var parser = new configParser(path);
+
+      it("should parse YAML.", function() {
+        assert.deepEqual(config, parser.parse());
+      });
     });
   });
 })
